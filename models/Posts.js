ad4616e130ef1abfd3adcad70caec411cf7881ed
dan-studio <danstudiocode@gmail.com>
2022-05-03 01:44:43 +0900

    post

--- models/Posts.js
+++ models/Posts.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose')
-const moment = require('moment')
+
 const PostsSchema = mongoose.Schema({
   sort: {type: String, required: [true, '제품 유형을 선택해 주세요!']},
   compName: { type: String, required: [true, '제조사를 입력해 주세요!'], trim: true},
   prodName: {type: String, required: [true, '제품명을 입력해 주세요!'], unique: true },
-  regDate: {type: Date, default: moment().format("YYYY-MM-DD hh:mm:ss")},
+  regDate: {type: Date, default: Date.now},
   author: {type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true}
 }, {
   collection: 'posts',
 })
-
+PostsSchema.post('save', function(error, doc, next) {
+  if (error.name === 'MongoError' && error.code === 11000) {
+    next(new Error('There was a duplicate key error'));
+  } else {
+    next();
+  }
+});
 const Posts = mongoose.model('post', PostsSchema)
 
 module.exports = Posts
